c9cb84f2193fa813639b6d8032a01182bebcaeae
hectorium110996 <hectori.um.15@gmail.com>
2020-03-22 22:45:17 -0600

    local y web

--- application/config/database.php
+++ application/config/database.php
@@ -5,6 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+//base de datos interna
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
@@ -26,3 +27,27 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+//base de datos externa
+/*
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'anglo',
+	'password' => 'anglo2020',
+	'database' => 'AngloLatinoDB',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => FALSE,
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);*/
