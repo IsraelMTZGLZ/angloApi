2d99a1eeb7618b776649d6a2cbf7e03b498ded7b
hectorium110996 <hectori.um.15@gmail.com>
2020-04-09 09:45:22 -0500

    nuevo

--- application/controllers/Aspirante/Api.php
+++ application/controllers/Aspirante/Api.php
@@ -108,6 +108,47 @@ class Api extends REST_Controller {
         $this->response($response,200);
     }
 
+    public function aspirante_get()
+    {
+        $id=$this->get('id');
+        if (count($this->get())>1) {
+            $response = array(
+                "status" => "error",
+                "status_code" => 409,
+                "message" => "Demasiados datos enviados",
+                "validations" =>array(
+                        "id"=>"Envia Id (get) para obtener un especifico articulo o vacio para obtener todos los articulos"
+                ),
+                "data"=>null
+            );
+        }else{
+            if ($id) {
+                $data = $this->DAO->selectEntity('Vw_Aspirante',array('usuario'=>$id),true);
+            }
+            else{
+                $data = $this->DAO->selectEntity('Vw_Aspirante',null,false);
+            }
+            if ($data) {
+                $response = array(
+                    "status" => "success",
+                    "status_code" => 201,
+                    "message" => "Articulo Cargado correctamente",
+                    "validations" =>null,
+                    "data"=>$data
+                );
+            }else{
+                $response = array(
+                    "status" => "error",
+                    "status_code" => 409,
+                    "message" => "No se recibio datos",
+                    "validations" =>null,
+                    "data"=>null
+                );
+            }
+        }
+        $this->response($response,200);
+    }
+
     function check_persona($str){
         if (!$str) {
             $this->form_validation->set_message('check_persona','The {field} campo es requerido');
