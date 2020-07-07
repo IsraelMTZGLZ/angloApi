d03a68698b76b019ed007d9879ef54946cf62ad2
IsraelMTZGLZ <israelmg250598@gmail.com>
2020-07-07 18:41:34 -0500

    DAO

--- application/models/DAO.php
+++ application/models/DAO.php
@@ -40,7 +40,24 @@ class DAO extends CI_Model{
 			 }
 	 }
 
-
+	 public function selectbyTwoEntity($entityName,$whereClause = null,$whereClauseTwo = null,$uniqueResult =false){
+ 		 if ($whereClause) {
+ 				 $this->db->where($whereClause);
+ 				 $this->db->where($whereClauseTwo);
+ 		 }
+ 		 $result = $this->db->get($entityName);
+ 		 if ($this->db->error()['message']!='') {
+ 				 return null;
+ 		 }
+ 		 else{
+ 				 if ($uniqueResult) {
+ 						 return $result->row();
+ 				 }
+ 				 else{
+ 						 return $result->result();
+ 				 }
+ 		 }
+  }
 	 function selectEntityVerano($entity, $whereClauseOne = null,$whereClauseTwo = null,$whereClauseThree = null){
 		 if($whereClauseOne and $whereClauseTwo and $whereClauseThree){
 		 $this->db->where($whereClauseOne);
@@ -98,6 +115,41 @@ class DAO extends CI_Model{
 
         return $response;
     }
+
+		public function updateByTwoData($entityName,$data,$whereClause,$whereClauseTwo)
+		{
+				$this->db->where($whereClause);
+				$this->db->where($whereClauseTwo);
+				$this->db->update($entityName,$data);
+				if ($this->db->error()['message']!='') {
+						$message = "";
+						switch ($this->db->error()['code']) {
+								case '1062':
+										$message="Item already inserted, please insert another one";
+										break;
+								case '1054':
+										$message="One column does not exist en databese";
+										break;
+								default:
+										$message = $this->db->error()['message'];
+										break;
+						}
+						$response = array(
+							"status"=>"error",
+							"status_code"=>201,
+							"message"=>$message
+						);
+				}
+				else{
+						$response = array(
+							"status"=>"success",
+							"status_code"=>201,
+							"message"=>"Artículo actualizado correctamente"
+						);
+				}
+
+				return $response;
+		}
 
 
     public function updateData($entityName,$data,$whereClause)
@@ -229,6 +281,33 @@ class DAO extends CI_Model{
 				$query = $this->db->get($entity);
 			}
 			return  $query->result();
+		}
+
+
+		//new
+		function deleteDataTwoClause($entityName,$whereClause,$whereClauseTwo){
+				$this->db->where($whereClause);
+				$this->db->where($whereClauseTwo);
+				$this->db->delete($entityName);
+				 if($this->db->error()['message']!=""){
+						$reponseDB = array(
+								"status"=>"error",
+								"status_code"=>409,
+								"message"=>"Db error: ".$this->db->error()['message'],
+								"validations"=>null,
+								"data"=>null
+						);
+				}else{
+						$reponseDB = array(
+								"status"=>"success",
+								"status_code"=>200,
+								"message"=>"Artículo eliminado correctamente",
+								"validations"=>null,
+								"data"=>null
+						);
+				}
+				return $reponseDB;
+
 		}
 
 
